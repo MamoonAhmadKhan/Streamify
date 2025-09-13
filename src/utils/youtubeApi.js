d0b2992fbb50cd17e8611256e4b2b27446eb72a9
MamoonAhmadKhan <khanmamoon887@gmail.com>
2025-09-13 18:04:45 +0530

    final

--- src/utils/youtubeApi.js
+++ src/utils/youtubeApi.js
@@ -1,4 +1,4 @@
-const GOOGLE_API_KEY = "AIzaSyDEfRMmfXq_MVFisoqMiV6vVVYPP8rnr80";
+const GOOGLE_API_KEY = import.meta.env.VITE_GOOGLE_API_KEY;
 
 export const YT_API = `https://youtube.googleapis.com/youtube/v3/videos?part=snippet%2CcontentDetails%2Cstatistics&chart=mostPopular&maxResults=50&regionCode=US&key=${GOOGLE_API_KEY}`;
 
